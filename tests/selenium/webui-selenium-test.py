1c969f34e240a9f39a3a0992af907ef621e90e59
[Aron Schueler] <aron.schueler@dass-it.de>
2018-01-18 16:36:49 +0100

    Improves readability, minor improvements of code.

--- tests/selenium/webui-selenium-test.py
+++ tests/selenium/webui-selenium-test.py
@@ -1,80 +1,108 @@
+#!/usr/bin/env python
+
 # -*- coding: utf-8 -*-
-import logging, os, re, sys, time, unittest
+
+# selenium.common.exceptions.ElementNotInteractableException: requires >= selenium-3.4.0
+
+import logging, os, re, sys, unittest
+from datetime import datetime, timedelta
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.common.exceptions import WebDriverException, ElementNotInteractableException, ElementNotVisibleException, TimeoutException, NoAlertPresentException, NoSuchElementException
+from selenium.common.exceptions import *
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select, WebDriverWait
+from time import sleep
+
+
 
 class WebuiSeleniumTest(unittest.TestCase):
 
+    browser = 'firefox'
+    base_url = "http://127.0.0.1/bareos-webui"
+    username = "admin"
+    password = "secret"
+    client = 'bareos-fd'
+    restorefile = '/usr/sbin/bconsole'
+    # slow down test for debugging
+    sleeptime = 0.0
+    # max seconds to wait for an element
+    maxwait = 10
+    # time to wait before trying again
+    waittime = 0.1
+
     def setUp(self):
-        if browser == "chrome":
-            d = DesiredCapabilities.CHROME
-            d['loggingPrefs'] = { 'browser':'ALL' }
-            self.driver = webdriver.Chrome('/usr/local/sbin/chromedriver')
-        if browser == "firefox":
+        self.logger = logging.getLogger()
+
+        if self.browser == "chrome":
+            #d = DesiredCapabilities.CHROME
+            #d['loggingPrefs'] = { 'browser':'ALL' }
+            self.driver = webdriver.Chrome('/usr/local/lib/chromium-browser/chromedriver')
+        if self.browser == "firefox":
             d = DesiredCapabilities.FIREFOX
             d['loggingPrefs'] = { 'browser':'ALL' }
             fp = webdriver.FirefoxProfile()
             fp.set_preference('webdriver.log.file', os.getcwd() + '/firefox_console.log')
-            self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(4)
-        self.base_url = "http://%s" % targethost
+            self.driver = webdriver.Firefox(capabilities=d, firefox_profile=fp)
+
+        # take base url, but remove last /
+        self.base_url = base_url.rstrip('/')
+
         self.verificationErrors = []
         self.accept_next_alert = True
-    
+
+
+
     def test_login(self):
         self.login()
-        time.sleep(0.5+t)
         self.logout()
+
+
 
     def test_menue(self):
         driver = self.driver
-        # On windows we have a different baseurl
-        if os.getenv('DIST') == "windows":
-            driver.get(self.base_url + "/")
-        else:
-            driver.get(self.base_url + "/bareos-webui/")
+
+        self.driver.get(self.base_url + "/")
 
         self.login()
-        self.wait_for_url('/bareos-webui/director/').click()
-        self.wait_for_url("/bareos-webui/schedule/").click()
-        self.wait_for_url("/bareos-webui/schedule/status/").click()
-        self.wait_for_url("/bareos-webui/storage/").click()
-        self.wait_for_url("/bareos-webui/client/").click()
-        self.wait_for_url("/bareos-webui/restore/").click()
+        self.wait_for_url_and_click('/director/')
+        self.wait_for_url_and_click("/schedule/")
+        self.wait_for_url_and_click("/schedule/status/")
+        self.wait_for_url_and_click("/storage/")
+        self.wait_for_url_and_click("/client/")
+        self.wait_for_url_and_click("/restore/")
         self.logout()
 
+
+
     def test_restore(self):
-        pathlist = restorefile.split('/')
-        driver = self.driver
 
         # LOGGING IN:
         self.login()
 
         # CHANGING TO RESTORE TAB:
-        self.wait_for_url("/bareos-webui/restore/").click()
+        self.wait_for_url_and_click("/restore/")
         
         # SELECTING CLIENT:
         # Selects the correct client
-        self.wait_for_element(By.XPATH, "(//button[@type='button'])[3]").click()
-        self.driver.find_element(By.LINK_TEXT, client).click()
+        self.wait_and_click(By.XPATH, "(//button[@data-id='client'])")
+        self.wait_and_click(By.LINK_TEXT, self.client)
         
         # FILE-SELECTION:
         # Clicks on file and navigates through the tree
         # by using the arrow-keys.
+        pathlist = self.restorefile.split('/')
         for i in pathlist[:-1]:
             self.wait_for_element(By.XPATH, "//a[contains(text(),'%s/')]" % i).send_keys(Keys.ARROW_RIGHT)
-        else:
-            self.wait_for_element(By.XPATH, "//a[contains(text(),'%s')]" % pathlist[-1]).click()
+        self.wait_for_element(By.XPATH, "//a[contains(text(),'%s')]" % pathlist[-1]).click()
 
         # CONFIRMATION:
         # Clicks on 'submit'
-        self.wait_for_element(By.XPATH, "//input[@id='submit']").click()
+        self.wait_and_click(By.XPATH, "//input[@id='submit']")
         # Confirms alert that has text "Are you sure ?"
         self.assertRegexpMatches(self.close_alert_and_get_its_text(), r"^Are you sure[\s\S]$")
+
+        # TODO: next modal, job run message
 
         # LOGOUT:
         self.logout()
@@ -83,61 +111,104 @@ class WebuiSeleniumTest(unittest.TestCase):
     def login(self):
         driver = self.driver
 
-        # on windows we have a different baseurl
-        if os.getenv('DIST') == "windows":
-            driver.get(self.base_url + "/auth/login")
-        else:
-            driver.get(self.base_url + "/bareos-webui/auth/login")
+        driver.get(self.base_url + "/auth/login")
         Select(driver.find_element_by_name("director")).select_by_visible_text("localhost-dir")
 
         driver.find_element_by_name("consolename").clear()
-        driver.find_element_by_name("consolename").send_keys(username)
+        driver.find_element_by_name("consolename").send_keys(self.username)
         driver.find_element_by_name("password").clear()
-        driver.find_element_by_name("password").send_keys(password)
+        driver.find_element_by_name("password").send_keys(self.password)
         driver.find_element_by_xpath("(//button[@type='button'])[2]").click()
         driver.find_element_by_link_text("English").click()
         driver.find_element_by_xpath("//input[@id='submit']").click()
-        while ("/bareos-webui/dashboard/" not in self.driver.current_url):
-            time.sleep(t*0.1)
+        while ("/dashboard/" not in self.driver.current_url):
+            sleep(self.waittime)
 
     def logout(self):
-        time.sleep(t)
-        self.wait_for_element(By.CSS_SELECTOR, "a.dropdown-toggle").click()
-        self.wait_for_element(By.LINK_TEXT, "Logout").click()
-        time.sleep(2)
+        # TODO:
+        # dismiss modals. Occurs when client with no jobs is selected first.
+        #try:
+            #self.logger.info("closing modal")
+            ##self.driver.switch_to_alert().dismiss()
+            #self.close_alert_and_get_its_text()
+            #time.sleep(t+5)
+        #except NoAlertPresentException:
+            #self.logger.info("skipped closing alert: none present")
+        self.wait_and_click(By.CSS_SELECTOR, "a.dropdown-toggle")
+        self.wait_and_click(By.LINK_TEXT, "Logout")
+        sleep(self.sleeptime)
+
+
 
     def wait_for_url(self, what):
         value="//a[contains(@href, '%s')]" % what
-        time.sleep(t*0.5)
         return self.wait_for_element(By.XPATH, value)
+
+
         
-    def wait_for_element(self, by, value):
-        i=10
-        element=None
-        while i>0 and element is None:
+    def wait_for_element(self, by, value, starttime = None):
+        logger = logging.getLogger()
+        element = None
+        if starttime is None:
+             starttime = datetime.now()
+        seconds = (datetime.now() - starttime).total_seconds()
+        logger.info("waiting for %s %s (%ds)", by, value, seconds)
+        while (seconds < self.maxwait) and (element is None):
             try:
-                time.sleep(0.5+(t*0.5))
-                logging.info("Loading %s", value)
                 tmp_element = self.driver.find_element(by, value)
                 if tmp_element.is_displayed():
                     element = tmp_element
             except ElementNotInteractableException:
-                logging.info("Element %s not interactable", value)
+                sleep(self.waittime)
+                logger.info("%s %s not interactable", by, value)
             except NoSuchElementException:
-                logging.info("Element %s not existing", value)
+                sleep(self.waittime)
+                logger.info("%s %s not existing", by, value)
             except ElementNotVisibleException:
-                logging.info("Element %s not visible", value)
-            i=i-1
-        if(i==0):
-            logging.info("Timeout while loading %s .", value)
+                sleep(self.waittime)
+                logger.info("%s %s not visible", by, value)
+            seconds = (datetime.now() - starttime).total_seconds()
+        if element:
+            logger.info("%s %s loaded after %ss." % (by, value, seconds))
+            sleep(self.sleeptime)
         else:
-            logging.info("Element loaded after %s seconds." % (11-i))
+            logger.warning("Timeout while loading %s %s (%d s)", by, value, seconds)
         return element
 
+
+    def wait_for_url_and_click(self, what):
+        value="//a[contains(@href, '%s')]" % what
+        return self.wait_and_click(By.XPATH, value)
+
+
+    def wait_and_click(self, by, value):
+        logger = logging.getLogger()
+        element = None
+        starttime = datetime.now()
+        seconds = 0.0
+        while (datetime.now() - starttime).total_seconds() < self.maxwait:
+            logging.info("waiting for %s %s (%ss)", by, value, seconds)
+            element = self.wait_for_element(by, value, starttime)
+            try:
+                element.click()
+            except WebDriverException:
+                sleep(self.waittime)
+                logger.info("WebDriverException while clicking %s %s", by, value)
+            else:
+                return element
+            seconds = (datetime.now() - starttime).total_seconds()
+        logger.error("failed to click %s %s", by, value)
+        return
+
+
     def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException as e: return False
+        try:
+            self.driver.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
         return True
+
+
 
     def close_alert_and_get_its_text(self):
         try:
@@ -148,7 +219,10 @@ class WebuiSeleniumTest(unittest.TestCase):
             else:
                 alert.dismiss()
             return alert_text
-        finally: self.accept_next_alert = True
+        except NoAlertPresentException:
+            pass
+        finally:
+            self.accept_next_alert = True
 
     def tearDown(self):
         self.driver.quit()
@@ -159,32 +233,39 @@ class WebuiSeleniumTest(unittest.TestCase):
 if __name__ == "__main__":
     # Configure the logger, for information about the timings set it to INFO
     # Selenium driver itself will write additional debug messages when set to DEBUG
-    logging.basicConfig(filename='webui-selenium-test.log', level=logging.INFO)
+    #logging.basicConfig(filename='webui-selenium-test.log', level=logging.DEBUG)
+    #logging.basicConfig(filename='webui-selenium-test.log', level=logging.INFO)
+    logging.basicConfig(filename='webui-selenium-test.log', format='%(levelname)s %(module)s.%(funcName)s: %(message)s', level=logging.INFO)
     logger = logging.getLogger()
 
     # Get attributes as environment variables,
     # if not available or set use defaults.
-    browser = os.environ.get('BROWSER')
-    if not browser:
-        browser = 'firefox'
-    client = os.environ.get('CLIENT')
-    if not client:
-        client = 'bareos-fd'
-    restorefile = os.environ.get('RESTOREFILE')
-    if not restorefile:
-        restorefile = '/usr/sbin/bconsole'
-    username = os.environ.get('USERNAME')
-    if not username:
-        username = "citest"
-    password = os.environ.get('PASSWORD')
-    if not password:
-        password = "citestpass"
-    t = os.environ.get('SLEEPTIME')
-    if t:
-        t = float(t)
-    if not t:
-        t = 1.0
-    targethost = os.environ.get('VM_IP')
-    if not targethost:
-        targethost = "127.0.0.1"
+    browser = os.environ.get('BAREOS_BROWSER')
+    if browser:
+        WebuiSeleniumTest.browser = browser
+
+    base_url = os.environ.get('BAREOS_BASE_URL')
+    if base_url:
+        WebuiSeleniumTest.base_url = base_url.rstrip('/')
+
+    username = os.environ.get('BAREOS_USERNAME')
+    if username:
+        WebuiSeleniumTest.username = username
+
+    password = os.environ.get('BAREOS_PASSWORD')
+    if password:
+        WebuiSeleniumTest.password = password
+
+    client = os.environ.get('BAREOS_CLIENT')
+    if client:
+        WebuiSeleniumTest.client = client
+
+    restorefile = os.environ.get('BAREOS_RESTOREFILE')
+    if restorefile:
+        WebuiSeleniumTest.restorefile = restorefile
+
+    sleeptime = os.environ.get('BAREOS_DELAY')
+    if sleeptime:
+        WebuiSeleniumTest.sleeptime = float(sleeptime)
+
     unittest.main()
